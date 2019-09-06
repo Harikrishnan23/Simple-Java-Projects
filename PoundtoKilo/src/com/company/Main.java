0f467a007dfd9df20b4a1feb5298257c74befd79
Hari Krishnan Thiagarajan <haribabu.krishnan634@gmail.com>
2019-09-06 21:41:47 +0100

    Simple java program to convert pounds to kilograms

---
+++ PoundtoKilo/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	double inputPound = 20;
+	double outputKilo = inputPound * 0.45359237;
+        System.out.println(" Result of pounds converted to kilograms: " + outputKilo);
+    }
+}
