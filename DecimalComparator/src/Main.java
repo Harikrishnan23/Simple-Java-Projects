11b4149f90edaa07b6abec5e87121690291dc50f
Hari Krishnan Thiagarajan <haribabu.krishnan634@gmail.com>
2019-09-11 20:41:16 +0100

    Comparison upto three decimal places

---
+++ DecimalComparator/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isEqual = DecimalComparator.areEqualByThreeDecimalPlaces(3.125, 3.125);
+        System.out.println(isEqual);
+    }
+}
